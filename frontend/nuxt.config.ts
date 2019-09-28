cd0f49421e4dca3dfd5a9fab25039bcf91af9bcf
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2019-09-28 13:05:23 +0900

    TypeScript の導入

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -37,7 +37,8 @@ module.exports = {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    '@nuxt/typescript-build'
   ],
   /*
    ** Nuxt.js modules
@@ -57,21 +58,7 @@ module.exports = {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+    customVariables: ['~/assets/variables.scss']
   },
   /*
    ** Build configuration
@@ -80,9 +67,6 @@ module.exports = {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  },
-  server: {
-    port: 8080
+    // extend(config, ctx) {}
   }
 }
