d0e176822be40d2683ec14710cb76dee80724625
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2019-10-06 11:29:14 +0900

    最低限ActionCableが動作するように修正

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,30 +1,30 @@
-const colors = require("vuetify/es5/util/colors").default;
-require("dotenv").config();
-const { API_SERVER_URL } = process.env;
+const colors = require('vuetify/es5/util/colors').default
+require('dotenv').config()
+const { API_SERVER_URL } = process.env
 
 module.exports = {
-  mode: "universal",
+  mode: 'spa',
   /*
    ** Headers of the page
    */
   head: {
-    titleTemplate: "%s - " + process.env.npm_package_name,
-    title: process.env.npm_package_name || "",
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: "#fff" },
+  loading: { color: '#fff' },
   /*
    ** Global CSS
    */
@@ -38,29 +38,29 @@ module.exports = {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    "@nuxtjs/eslint-module",
-    "@nuxtjs/vuetify",
-    "@nuxt/typescript-build"
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify',
+    '@nuxt/typescript-build'
   ],
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    "@nuxtjs/axios",
-    "@nuxtjs/pwa"
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa'
   ],
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
-  axios: { baseURL: API_SERVER_URL || "http://localhost:3000" },
+  axios: { baseURL: API_SERVER_URL || 'http://localhost:3000' },
   /*
    ** vuetify module configuration
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    customVariables: ["~/assets/variables.scss"]
+    customVariables: ['~/assets/variables.scss']
   },
   /*
    ** Build configuration
@@ -71,4 +71,4 @@ module.exports = {
      */
     // extend(config, ctx) {}
   }
-};
+}
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,75 +1,55 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <no-ssr>
+    <v-layout column justify-center align-center>
+      <v-flex xs12 sm8 md6>
+        <div>{{ this.saveMessage }}</div>
+        <form>
+          <v-text-field
+            v-model="messageText"
+            label="テキスト"
+            data-vv-name="messageText"
+            placeholder="何かテキストを入力してください"
+          />
+          <v-btn @click="handleClick" block class="font-weight-bold">投稿する</v-btn>
+        </form>
+      </v-flex>
+    </v-layout>
+  </no-ssr>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import ActionCable from 'actioncable'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {
+      messageText: '',
+      saveMessage: ''
+    }
+  },
+  created() {
+    const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
+
+    this.messageChannel = cable.subscriptions.create('PostChannel', {
+      received: (data) => {
+        this.saveMessage = data
+        // this.$store.commit('addMessage', data)
+      }
+    })
+  },
+  methods: {
+    handleClick: function() {
+      //ActionCable PostChannelにおけるpostメソッドを実行する
+      this.messageChannel.perform('post', {
+        message: this.messageText
+      })
+      // console.log(this.$store.state.messages);
+      //メッセージ追加後にテキストボックスを空にする
+      this.messageText = ''
+    }
   }
 }
 </script>
+
+<style lang="scss" module>
+</style>
